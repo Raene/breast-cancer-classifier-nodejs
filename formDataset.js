acbf60b5056d6c1ee6bc13e87eb338d6a345de1d
Raene <crofty.52.bu@gmail.com>
2020-01-19 13:42:45 +0100

    added comments

--- formDataset.js
+++ formDataset.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 const path = require("path");
-//let testArray = [];
+
 let filePath = path.join(__dirname, "wdbc.data");
-//testArray.push(data);
+
+//read the dataset into an array
 async function cleanDataset(encoding) {
   try {
     let data = await fs.promises.readFile(filePath, { encoding });
@@ -13,11 +14,14 @@ async function cleanDataset(encoding) {
   }
 }
 
+//formdataset
 async function formDataset() {
   try {
     let data = await cleanDataset("utf8");
     let testArray = [];
     let trainArray = [];
+
+    //create an array of objects from the dataset
     newData = data
       .map(el => {
         let nIndex = el.search(/B|M/);
@@ -28,14 +32,18 @@ async function formDataset() {
           class: metaData[1],
           imageData
         };
-      })
+      }) // filter any empty array
       .filter(el => el.imageData[0] !== "");
+
+    //split data into testArray and trainArray
     for (let i = 0; i < Math.round(newData.length / 10); i++) {
       index = Math.floor(Math.random() * Math.round(100)) + 1;
       testArray.push(newData[index]);
       newData.splice(index, 1);
       trainArray = newData;
     }
+
+    //write the arrays to their respective json files
     fs.writeFile("training.json", JSON.stringify(trainArray), function() {});
     fs.writeFile("testing.json", JSON.stringify(testArray), function() {});
   } catch (err) {}
